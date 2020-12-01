1f95b703e7a2e855fdb400d5628a78cbbb96513c
Carlos Vieira <cvieira.eduardo@gmail.com>
2020-12-01 02:05:27 -0300

    Alteração de fluxo do script e tratamento do output do nmap

--- script.py
+++ script.py
@@ -1,6 +1,42 @@
 from utils.colors import *
 import tools.nmap as nmap
+from utils.banner import *
 
-print(nmap.scan("192.168.1.15"))
-prRed("TESTE")
-print(nmap.os("192.168.1.15"))
+import sys
+import time
+
+
+def port_scan(host):
+	prGreen('[!] Scan iniciado para o host: ' + host)
+
+	result = nmap.scan(host)
+	prYellow('[+] Progresso: ' + result['summary'])
+	prGreen('[!] Status do Scan: ' + result['status'])
+
+	if(len(result['ports'])>0):
+		for port,data in result['ports'].items():
+			prGreen('[+] ' + str(port) + '/' + str(data['protocol']) + ' - ' + data['service'])
+	else:
+		prRed('[X] Nenhuma porta aberta encontrada :(')
+
+
+def detect_os(host):
+	 result = nmap.os(host)
+	 return result
+
+
+
+def main(host):
+	# começa com um scan de portas
+	port_scan(host)
+	# depois detecta o S.O
+	#detect_os(host)
+
+
+if __name__ == "__main__":
+	banner()
+	if(len(sys.argv) <= 1):
+		prYellow('[!] Use: python script.py IP')
+		prGreen('[>] Exemplo: python script.py 192.168.1.15')
+	else:
+		main(sys.argv[1])
--- tools/nmap.py
+++ tools/nmap.py
@@ -1,9 +1,22 @@
 import nmap3
-
+import json
 nmap = nmap3.Nmap()
 
 def scan(host):
-    return nmap.scan_top_ports(host, default=100)
-
+	result_filtered = {}
+	opened_ports = {}
+	result_raw = nmap.scan_top_ports(host, default=100)
+	result_filtered.update({'summary': result_raw['runtime']['summary']})
+	result_filtered.update({'status': result_raw['runtime']['exit']})
+	result_filtered.update({'host': list(result_raw.keys())[0]})
+	for ports in result_raw[list(result_raw.keys())[0]]:
+		if('state' in ports):
+			if(ports['state'] == 'open'):
+				opened_ports[ports['portid']] = {'portid' : ports['portid'], 'state': ports['state'], 'protocol': ports['protocol'], 'service': ports['service']['name']}
+	result_filtered.update({'ports': opened_ports})
+	if(result_filtered['status'] == 'success'):
+		return result_filtered
+	else:
+		return False
 def os(host):
     return nmap.nmap_os_detection(host)
